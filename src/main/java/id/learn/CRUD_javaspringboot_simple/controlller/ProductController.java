dea7cbc5772a4b1a837a414751fdd98d0eb57a7f
engkos kostaman <engkos.kostaman@bni.co.id>
2024-09-04 15:16:02 +0700

    crud java springboot simple

--- src/main/java/id/learn/CRUD_javaspringboot_simple/controlller/ProductController.java
+++ src/main/java/id/learn/CRUD_javaspringboot_simple/controlller/ProductController.java
@@ -17,38 +17,37 @@ public class ProductController {
 @Autowired
 private ProductService productService;
 
+//api get all
 @GetMapping
 public ResponseEntity<List<Product>> getAllProducts() {
 return new ResponseEntity<>(productService.findAllProducts(),
 HttpStatus.OK);
 }
 
+//api create 
 @PostMapping
-public ResponseEntity<Product> saveProduct(@Valid @RequestBody Product
-product) {
+public ResponseEntity<Product> saveProduct(@Valid @RequestBody Product product) {
 return new ResponseEntity<>(productService.saveorUpdateProduct(product),
 HttpStatus.OK);
 }
 
-@PutMapping("{id}")
-public ResponseEntity<Product> updateUser(@PathVariable("id") Long productId,
-  @Valid @RequestBody Product product){
+//api edit 
+@PutMapping("/{id}")
+public ResponseEntity<Product> updateUser(@PathVariable("id") Long productId,@Valid @RequestBody Product product){
     product.setId(productId);
     return new ResponseEntity<Product>(productService.updateProduct(product),
     HttpStatus.OK
     );
 }
 
-
-
-
+// api get by id
 @GetMapping("/{id}")
-public ResponseEntity<Product> getOneProduct(@PathVariable Long id) throws
-Exception {
+public ResponseEntity<Product> getOneProduct(@PathVariable Long id) throws Exception {
 return new ResponseEntity<Product>(productService.findProductById(id),
 HttpStatus.OK);
 }
 
+//api delete
 @DeleteMapping("/{id}")
 public String deleteProduct(@PathVariable Long id) {
 productService.deleteProduct(id);
